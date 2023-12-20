b57c40c2362b6ec512db85f3b27d744d04977767
Zach Quinn <ZachQuinn@sopheon.com>
2023-12-19 22:24:43 -0700

    crude data display

--- nextjs-dashboard/app/dashboard/search/page.tsx
+++ nextjs-dashboard/app/dashboard/search/page.tsx
@@ -1,13 +1,13 @@
 //"use client"
 
-import { dozerDummyData } from '@/app/lib/api'  // TODO: fix CORS issue and fetch data
+import { dozerDummyData } from '@/app/lib/api'; // TODO: fix CORS issue and fetch data
 import DozerCard from '@/app/ui/DozerCard';
-import { useState, useEffect } from 'react'
- 
+import { useState, useEffect } from 'react';
+
 export default function Page() {
   // const [data, setData] = useState(null)
   // const [isLoading, setLoading] = useState(true)
- 
+
   // useEffect(() => {
   //   console.log('about to fetch');
   //   fetch('https://www.cat.com/content/catdotcom/en_US/products/new/equipment/dozers/jcr:content/root/responsivegrid/productcards.feed.json',
@@ -21,13 +21,18 @@ export default function Page() {
   //       setLoading(false)
   //     })
   // }, [])
- 
+
   // if (isLoading) return <p>Loading...</p>
   // if (!data) return <p>No profile data</p>
-  const listItems = dozerDummyData.models.map((m) =>
-    <DozerCard key={m.modelId} modelName={m.model_name}></DozerCard>
-  );
-  return (
-    listItems
-  )
+  const listItems = dozerDummyData.models.map((m) => (
+    <DozerCard
+      key={m.modelId}
+      makeName={m.brand}
+      modelName={m.model_name}
+      category={m.productCategory}
+      engineHp={m.specs[1].spec_value[0]} // TODO: extract based on key name
+      operatingWeight={m.specs[2].spec_value[0]}
+    ></DozerCard>
+  ));
+  return listItems;
 }
--- nextjs-dashboard/app/ui/DozerCard.tsx
+++ nextjs-dashboard/app/ui/DozerCard.tsx
@@ -1,3 +1,25 @@
-export default function Page({modelName} : { modelName: string}) {
-    return <div>{modelName}</div>;
+export interface DozerCardProps {
+  makeName: string;
+  modelName: string;
+  category: string;
+  engineHp: string; // TODO: number
+  operatingWeight: string;  // TODO: number
+}
+
+export default function DozerCard({
+  makeName,
+  modelName,
+  category,
+  engineHp,
+  operatingWeight,
+}: DozerCardProps) {
+  return (
+    <div>
+      <div>Make: {makeName}</div>
+      <div>Model: {modelName}</div>
+      <div>Category: {category}</div>
+      <div>Engine HP: {engineHp}</div>
+      <div>Operating weight: {operatingWeight}</div>
+    </div>
+  );
 }
