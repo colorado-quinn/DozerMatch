b94bd598341fc929003b2452f5c4749ee5037960
Zach Quinn <ZachQuinn@sopheon.com>
2023-12-20 20:44:54 -0700

    move image

--- nextjs-dashboard/app/ui/search/DozerCard.tsx
+++ nextjs-dashboard/app/ui/search/DozerCard.tsx
@@ -1,4 +1,4 @@
-import { Card, CardActionArea, CardContent } from '@mui/material';
+import { Box, Card, CardActionArea, CardContent } from '@mui/material';
 import Image from 'next/image';
 
 export interface DozerInfo {
@@ -17,12 +17,20 @@ export interface DozerCardProps {
   onDozerClick: (dozerInfo: DozerInfo) => void;
 }
 
-export default function DozerCard({dozer, onDozerClick}: DozerCardProps) {
+export default function DozerCard({ dozer, onDozerClick }: DozerCardProps) {
   return (
     <div className="cardContainer">
       <Card>
         <CardActionArea onClick={() => onDozerClick(dozer)}>
           <CardContent>
+            <Box display={'flex'} justifyContent={'center'}>
+              <Image
+                src={dozer.imageUrl}
+                width={200}
+                height={200}
+                alt={`Picture of dozer ${dozer.makeName} - ${dozer.modelName}`}
+              />
+            </Box>
             <div>Make: {dozer.makeName}</div>
             <div>Model: {dozer.modelName}</div>
             <div>Category: {dozer.category}</div>
@@ -30,12 +38,6 @@ export default function DozerCard({dozer, onDozerClick}: DozerCardProps) {
             <div>Operating weight: {dozer.operatingWeightString}</div>
             {/* <div>Engine HP: {engineHp}</div>
           <div>Operating weight: {operatingWeight}</div> */}
-            <Image
-              src={dozer.imageUrl}
-              width={200}
-              height={200}
-              alt={`Picture of dozer ${dozer.makeName} - ${dozer.modelName}`}
-            />
           </CardContent>
         </CardActionArea>
       </Card>
