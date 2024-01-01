4821355ee480fd9f5386ec9930d132ad8ae91be6
Zach Quinn <ZachQuinn@sopheon.com>
2024-01-01 16:05:27 -0700

    update and theme logo

--- nextjs-dashboard/app/ui/acme-logo.tsx
+++ nextjs-dashboard/app/ui/acme-logo.tsx
@@ -1,13 +1,13 @@
-import { GlobeAltIcon } from '@heroicons/react/24/outline';
+import { WrenchScrewdriverIcon } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
 
 export default function AcmeLogo() {
   return (
     <div
-      className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
+      className={`${lusitana.className} flex md:flex-col leading-none text-white`}
     >
-      <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
-      <p className="text-[44px]">Acme</p>
+      <WrenchScrewdriverIcon className="h-12 w-12 mr-5" />
+      <p className="text-[44px]">Dozer Match</p>
     </div>
   );
 }
